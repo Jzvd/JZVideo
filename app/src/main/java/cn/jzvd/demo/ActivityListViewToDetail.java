c2997288fba251399792e88b493ab0e423acf56a
Liberation <qq18159020773@gmail.com>
2019-12-28 20:06:36 +0800

    Add auto play when at WIFI state

--- app/src/main/java/cn/jzvd/demo/ActivityListViewToDetail.java
+++ app/src/main/java/cn/jzvd/demo/ActivityListViewToDetail.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
@@ -52,6 +53,14 @@ public class ActivityListViewToDetail extends AppCompatActivity {
             }
         });
         recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    AutoPlayUtils.onScrollPlayVideo(recyclerView, mLayoutManager.findFirstVisibleItemPosition(), mLayoutManager.findLastVisibleItemPosition());
+                }
+            }
+
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
