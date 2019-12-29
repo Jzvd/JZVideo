12141b89debf0b3b00b1b9500d0ab0c247a8d2d3
zhaodaizheng <709136855@qq.com>
2019-12-29 18:02:18 +0800

    add tiktok

---
+++ app/src/main/java/cn/jzvd/demo/tiktok/ActivityTikTok.java
@@ -0,0 +1,124 @@
+package cn.jzvd.demo.tiktok;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.OrientationHelper;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.view.View;
+
+import cn.jzvd.Jzvd;
+import cn.jzvd.demo.CustomJzvd.JzvdStdTikTok;
+import cn.jzvd.demo.R;
+import cn.jzvd.demo.widget.OnViewPagerListener;
+import cn.jzvd.demo.widget.ViewPagerLayoutManager;
+
+public class ActivityTikTok extends AppCompatActivity {
+
+    private RecyclerView rvTiktok;
+    private AdapterTikTokRecyclerView mAdapter;
+    private ViewPagerLayoutManager mViewPagerLayoutManager;
+    private JzvdStdTikTok mVideoPlayer;
+    private int mCurrentPosition = -1;
+
+    public static void openActivity(Context context) {
+        Intent intent = new Intent(context, ActivityTikTok.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayUseLogoEnabled(false);
+        getSupportActionBar().setTitle("TikTokRecyclerView");
+
+        setContentView(R.layout.activity_tiktok);
+        rvTiktok = findViewById(R.id.rv_tiktok);
+
+        mAdapter = new AdapterTikTokRecyclerView(this);
+        mViewPagerLayoutManager = new ViewPagerLayoutManager(this, OrientationHelper.VERTICAL);
+        rvTiktok.setLayoutManager(mViewPagerLayoutManager);
+        rvTiktok.setAdapter(mAdapter);
+
+        mViewPagerLayoutManager.setOnViewPagerListener(new OnViewPagerListener() {
+            @Override
+            public void onInitComplete() {
+                //自动播放第一条
+                autoPlayVideo(0);
+            }
+
+            @Override
+            public void onPageRelease(boolean isNext, int position) {
+                if (mCurrentPosition == position) {
+                    Jzvd.releaseAllVideos();
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position, boolean isBottom) {
+                if (mCurrentPosition == position){
+                    return;
+                }
+                autoPlayVideo(position);
+                mCurrentPosition = position;
+            }
+        });
+
+        rvTiktok.addOnChildAttachStateChangeListener(new RecyclerView.OnChildAttachStateChangeListener() {
+            @Override
+            public void onChildViewAttachedToWindow(View view) {
+
+            }
+
+            @Override
+            public void onChildViewDetachedFromWindow(View view) {
+                Jzvd jzvd = view.findViewById(R.id.videoplayer);
+                if (jzvd != null && Jzvd.CURRENT_JZVD != null &&
+                        jzvd.jzDataSource.containsTheUrl(Jzvd.CURRENT_JZVD.jzDataSource.getCurrentUrl())) {
+                    if (Jzvd.CURRENT_JZVD != null && Jzvd.CURRENT_JZVD.screen != Jzvd.SCREEN_FULLSCREEN) {
+                        Jzvd.releaseAllVideos();
+                    }
+                }
+            }
+        });
+    }
+
+    private void autoPlayVideo(int postion) {
+        if (rvTiktok == null || rvTiktok.getChildAt(0) == null){
+            return;
+        }
+        JzvdStdTikTok player = rvTiktok.getChildAt(0).findViewById(R.id.videoplayer);
+        if (player != null) {
+            player.startVideoAfterPreloading();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (Jzvd.backPress()) {
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Jzvd.releaseAllVideos();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
