6d68df92992385c98f625636bee1f6e600014126
fiui <910689331@qq.com>
2019-12-31 17:48:42 +0800

    Add auto play demo

--- app/src/main/java/cn/jzvd/demo/ActivityListViewAutoPlay.java
+++ app/src/main/java/cn/jzvd/demo/ActivityListViewAutoPlay.java
@@ -1,59 +1,63 @@
 package cn.jzvd.demo;
 
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SimpleItemAnimator;
 import android.view.MenuItem;
+import android.view.View;
 
 import cn.jzvd.Jzvd;
 import cn.jzvd.demo.CustomJzvd.AutoPlayUtils;
-import cn.jzvd.demo.CustomJzvd.ViewAttr;
 
-/**
- * 列表平滑进入详情页
- */
-public class ActivityListViewToDetail extends AppCompatActivity {
+
+public class ActivityListViewAutoPlay extends AppCompatActivity {
     RecyclerView recyclerView;
-    AdapterSmoothRecyclerView adapterVideoList;
-    private static final String TAG = "ActivityListViewToDetai";
-    public static ActivityListViewToDetail activityListViewToDetail;
+    AdapterRecyclerView adapterVideoList;
+    private LinearLayoutManager mLayoutManager;
 
+    @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setDisplayUseLogoEnabled(false);
-        getSupportActionBar().setTitle("ActivityListViewToDetail");
+        getSupportActionBar().setTitle("RecyclerView");
         setContentView(R.layout.activity_recyclerview_content);
-        activityListViewToDetail = this;
+
         recyclerView = findViewById(R.id.recyclerview);
-        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(mLayoutManager);
-        adapterVideoList = new AdapterSmoothRecyclerView(this);
+
+        adapterVideoList = new AdapterRecyclerView(this);
         recyclerView.setAdapter(adapterVideoList);
-        adapterVideoList.setOnVideoClick(new AdapterSmoothRecyclerView.OnVideoClick() {
-            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        recyclerView.addOnChildAttachStateChangeListener(new RecyclerView.OnChildAttachStateChangeListener() {
             @Override
-            public void videoClick(ViewAttr viewAttr, int position) {
-                Intent intent = new Intent(ActivityListViewToDetail.this, ActivityListViewDetail.class);
-                intent.putExtra("attr", viewAttr);
-                startActivity(intent);
+            public void onChildViewAttachedToWindow(View view) {
+
+            }
+
+            @Override
+            public void onChildViewDetachedFromWindow(View view) {
+                Jzvd jzvd = view.findViewById(R.id.videoplayer);
+                if (jzvd != null && Jzvd.CURRENT_JZVD != null &&
+                        jzvd.jzDataSource.containsTheUrl(Jzvd.CURRENT_JZVD.jzDataSource.getCurrentUrl())) {
+                    if (Jzvd.CURRENT_JZVD != null && Jzvd.CURRENT_JZVD.screen != Jzvd.SCREEN_FULLSCREEN) {
+                        Jzvd.releaseAllVideos();
+                    }
+                }
             }
         });
+
         recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
                 super.onScrollStateChanged(recyclerView, newState);
                 if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                    AutoPlayUtils.onScrollPlayVideo(recyclerView,R.id.jzvdplayer, mLayoutManager.findFirstVisibleItemPosition(), mLayoutManager.findLastVisibleItemPosition());
+                    AutoPlayUtils.onScrollPlayVideo(recyclerView, R.id.videoplayer, mLayoutManager.findFirstVisibleItemPosition(), mLayoutManager.findLastVisibleItemPosition());
                 }
             }
 
@@ -65,12 +69,6 @@ public class ActivityListViewToDetail extends AppCompatActivity {
                 }
             }
         });
-        ((SimpleItemAnimator)recyclerView.getItemAnimator()).setSupportsChangeAnimations(false);
-    }
-
-
-    public void animateFinish(){
-        adapterVideoList.notifyDataSetChanged();
     }
 
     @Override
@@ -81,6 +79,11 @@ public class ActivityListViewToDetail extends AppCompatActivity {
         super.onBackPressed();
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Jzvd.releaseAllVideos();
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -92,11 +95,4 @@ public class ActivityListViewToDetail extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        activityListViewToDetail = null;
-        AutoPlayUtils.positionInList = -1;
-        Jzvd.releaseAllVideos();
-    }
 }
