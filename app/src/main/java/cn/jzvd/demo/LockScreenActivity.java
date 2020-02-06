7e4fa2f88985cf065d9578883a4fce84cb85af81
chenxingwang <848864817@qq.com>
2020-02-06 20:36:40 +0800

    add lockscreen demo

---
+++ app/src/main/java/cn/jzvd/demo/LockScreenActivity.java
@@ -0,0 +1,38 @@
+package cn.jzvd.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+import cn.jzvd.Jzvd;
+import cn.jzvd.demo.CustomJzvd.LockScreen;
+
+
+public class LockScreenActivity extends AppCompatActivity {
+    LockScreen lockScreen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock_screen);
+        lockScreen = findViewById(R.id.ls);
+        lockScreen.setUp("http://jzvd.nathen.cn/342a5f7ef6124a4a8faf00e738b8bee4/cf6d9db0bd4d41f59d09ea0a81e918fd-5287d2089db37e62345123a1be272f8b.mp4"
+                , "饺子快长大");
+        Glide.with(this).load("http://jzvd-pic.nathen.cn/jzvd-pic/1bb2ebbe-140d-4e2e-abd2-9e7e564f71ac.png").into(lockScreen.thumbImageView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Jzvd.releaseAllVideos();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (Jzvd.backPress()) {
+            return;
+        }
+        super.onBackPressed();
+    }
+}
