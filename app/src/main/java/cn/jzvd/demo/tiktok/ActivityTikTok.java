33194524323dfe9be856ca0e656ecf3489be6018
Nathen <1066666651@qq.com>
2020-02-17 01:24:47 +0800

    fix fullscreen not build but still have bug

--- app/src/main/java/cn/jzvd/demo/tiktok/ActivityTikTok.java
+++ app/src/main/java/cn/jzvd/demo/tiktok/ActivityTikTok.java
@@ -3,6 +3,8 @@ package cn.jzvd.demo.tiktok;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.OrientationHelper;
@@ -25,6 +27,9 @@ public class ActivityTikTok extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_tiktok);
         rvTiktok = findViewById(R.id.rv_tiktok);
