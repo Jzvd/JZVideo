6f9c3dd9ea1ead88c00e0e825711f568a7b8d1fa
lipangit <1066666651@qq.com>
2020-12-13 23:15:18 +0800

    kt urls

---
+++ demo/src/main/java/org/jzvd/jzvideo/urls.kt
@@ -0,0 +1,64 @@
+package org.jzvd.jzvideo
+
+
+val ldjVideos = arrayOf(
+    "http://videos.jzvd.org/ldj/01-ldj.mp4",
+    "http://videos.jzvd.org/ldj/02-ldj.mp4",
+    "http://videos.jzvd.org/ldj/03-ldj.mp4",
+    "http://videos.jzvd.org/ldj/04-ldj.mp4",
+    "http://videos.jzvd.org/ldj/05-ldj.mp4",
+    "http://videos.jzvd.org/ldj/06-ldj.mp4",
+    "http://videos.jzvd.org/ldj/07-ldj.mp4",
+    "http://videos.jzvd.org/ldj/08-ldj.mp4",
+    "http://videos.jzvd.org/ldj/09-ldj.mp4",
+    "http://videos.jzvd.org/ldj/10-ldj.mp4",
+    "http://videos.jzvd.org/ldj/11-ldj.mp4",
+    "http://videos.jzvd.org/ldj/12-ldj.mp4",
+    "http://videos.jzvd.org/ldj/13-ldj.mp4",
+    "http://videos.jzvd.org/ldj/14-ldj.mp4",
+    "http://videos.jzvd.org/ldj/15-ldj.mp4",
+    "http://videos.jzvd.org/ldj/16-ldj.mp4",
+    "http://videos.jzvd.org/ldj/17-ldj.mp4",
+    "http://videos.jzvd.org/ldj/18-ldj.mp4",
+    "http://videos.jzvd.org/ldj/19-ldj.mp4",
+    "http://videos.jzvd.org/ldj/20-ldj.mp4",
+    "http://videos.jzvd.org/ldj/21-ldj.mp4",
+    "http://videos.jzvd.org/ldj/22-ldj.mp4",
+    "http://videos.jzvd.org/ldj/23-ldj.mp4",
+    "http://videos.jzvd.org/ldj/24-ldj.mp4",
+    "http://videos.jzvd.org/ldj/25-ldj.mp4",
+    "http://videos.jzvd.org/ldj/26-ldj.mp4",
+    "http://videos.jzvd.org/ldj/27-ldj.mp4",
+    "http://videos.jzvd.org/ldj/28-ldj.mp4",
+    "http://videos.jzvd.org/ldj/29-ldj.mp4",
+    "http://videos.jzvd.org/ldj/30-ldj.mp4",
+    "http://videos.jzvd.org/ldj/31-ldj.mp4",
+    "http://videos.jzvd.org/ldj/32-ldj.mp4",
+    "http://videos.jzvd.org/ldj/33-ldj.mp4",
+    "http://videos.jzvd.org/ldj/34-ldj.mp4",
+    "http://videos.jzvd.org/ldj/35-ldj.mp4",
+    "http://videos.jzvd.org/ldj/36-ldj.mp4",
+    "http://videos.jzvd.org/ldj/37-ldj.mp4",
+    "http://videos.jzvd.org/ldj/38-ldj.mp4",
+    "http://videos.jzvd.org/ldj/39-ldj.mp4",
+    "http://videos.jzvd.org/ldj/40-ldj.mp4",
+    "http://videos.jzvd.org/ldj/41-ldj.mp4",
+    "http://videos.jzvd.org/ldj/42-ldj.mp4",
+    "http://videos.jzvd.org/ldj/43-ldj.mp4",
+    "http://videos.jzvd.org/ldj/44-ldj.mp4",
+    "http://videos.jzvd.org/ldj/45-ldj.mp4"
+)
+
+val videos = arrayOf(
+    "",
+    ""
+)
+
+val thumbnails = arrayOf(
+    "",
+    ""
+)
+
+val someUrls = ldjVideos.copyOfRange(3, 9)
+
+
