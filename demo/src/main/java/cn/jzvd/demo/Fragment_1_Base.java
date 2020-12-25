135f4c8fad7180d904d8aa68f1b720a390c4960a
lipangit <1066666651@qq.com>
2020-12-26 05:03:30 +0800

    change server

--- demo/src/main/java/cn/jzvd/demo/Fragment_1_Base.java
+++ demo/src/main/java/cn/jzvd/demo/Fragment_1_Base.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -39,25 +40,30 @@ public class Fragment_1_Base extends Fragment implements View.OnClickListener {
     private JzvdStd mJzvdStd;
     private Button mOrientation,
             mRotationAndVideoSize, mCustomMediaPlayer, mPreLoading, mScreenRotate;
+    private Button serverCn, serverUs;
     private Jzvd.JZAutoFullscreenListener mSensorEventListener;
     private SensorManager mSensorManager;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = View.inflate(getContext(), R.layout.fragment_api, null);
+        View view = View.inflate(getContext(), R.layout.fragment_base, null);
         mJzvdStd = view.findViewById(R.id.jz_video);
         mOrientation = view.findViewById(R.id.orientation);
         mRotationAndVideoSize = view.findViewById(R.id.rotation_and_videosize);
         mCustomMediaPlayer = view.findViewById(R.id.custom_mediaplayer);
         mPreLoading = view.findViewById(R.id.preloading);
         mScreenRotate = view.findViewById(R.id.screen_rotate);
+        serverCn = view.findViewById(R.id.server_cn);
+        serverUs = view.findViewById(R.id.server_us);
 
         mOrientation.setOnClickListener(this);
         mRotationAndVideoSize.setOnClickListener(this);
         mCustomMediaPlayer.setOnClickListener(this);
         mPreLoading.setOnClickListener(this);
         mScreenRotate.setOnClickListener(this);
+        serverCn.setOnClickListener(this);
+        serverUs.setOnClickListener(this);
 
         mSensorManager = (SensorManager) getContext().getSystemService(SENSOR_SERVICE);
         mSensorEventListener = new Jzvd.JZAutoFullscreenListener();
@@ -120,6 +126,14 @@ public class Fragment_1_Base extends Fragment implements View.OnClickListener {
                 break;
             case R.id.screen_rotate:
                 startActivity(new Intent(getContext(), ScreenRotateActivity.class));
+                break;
+            case R.id.server_cn:
+                UrlsKt.setServer_name(UrlsKt.getCn());
+                Toast.makeText(getContext(), "change server to: " + UrlsKt.getCn(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.server_us:
+                UrlsKt.setServer_name(UrlsKt.getUs());
+                Toast.makeText(getContext(), "change server to: " + UrlsKt.getUs(), Toast.LENGTH_SHORT).show();
                 break;
         }
     }
